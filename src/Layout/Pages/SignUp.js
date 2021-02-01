e9bac30d3ac69646c5d528a3c7751e72ebe96852
Sanam Kapoor <47074142+sanamKapoor@users.noreply.github.com>
2021-02-01 17:58:44 +0500

    Update SignUp.js

--- src/Layout/Pages/SignUp.js
+++ src/Layout/Pages/SignUp.js
@@ -80,7 +80,7 @@ function SignUp() {
         formData.append('password', password);
         formData.append('image', image);
 
-        dispatch(backendReqModal('/auth/register', 'POST', formData, { 'Content-Type': null }))
+        dispatch(backendReqModal('/auth/register', 'POST', formData, { 'Content-Type': 'multipart/form-data' }))
 
     }
 
